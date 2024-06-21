3b6cecaf2828c399a2d239452d99ab769857de28
invalid w <wangjuesix@gmail.com>
2024-06-21 09:49:58 +0800

    chore: init

--- modern.config.ts
+++ modern.config.ts
@@ -1,7 +1,8 @@
 import { moduleTools, defineConfig } from '@modern-js/module-tools';
 import { modulePluginDoc } from '@modern-js/plugin-rspress';
+import { testingPlugin } from '@modern-js/plugin-testing';
 
 export default defineConfig({
-  plugins: [moduleTools(), modulePluginDoc()],
+  plugins: [moduleTools(), modulePluginDoc(), testingPlugin()],
   buildPreset: 'npm-library',
 });
